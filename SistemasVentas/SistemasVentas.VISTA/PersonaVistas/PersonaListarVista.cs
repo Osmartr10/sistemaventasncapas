116e87f8a9158b805129ab0da9b5236093e8d579
Osmar <osmar.tito@brightsoft.io>
2024-03-15 10:03:40 -0400

    seleccionar

---
+++ SistemasVentas/SistemasVentas.VISTA/PersonaVistas/PersonaListarVista.cs
@@ -0,0 +1,32 @@
+﻿using SistemasVentas.BSS;
+using SistemasVentas.VISTA.UsuarioVistas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemasVentas.VISTA.PersonaVistas
+{
+    public partial class PersonaListarVista : Form
+    {
+        public PersonaListarVista()
+        {
+            InitializeComponent();
+        }
+        PersonaBss bss = new PersonaBss();
+        private void PersonaListarVista_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bss.ListarPersonaBss();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UsuarioInsertarVista.IdPersonaSeleccionada = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+        }
+    }
+}
---
+++ SistemasVentas/SistemasVentas.VISTA/UsuarioVistas/UsuarioInsertarVista.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SistemasVentas.VISTA.UsuarioVistas
+{
+    partial class UsuarioInsertarVista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 0;
+            label1.Text = "PERSONA";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(84, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(183, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(273, 29);
+            button1.Name = "button1";
+            button1.Size = new Size(132, 23);
+            button1.TabIndex = 2;
+            button1.Text = "SELECCIONAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 15);
+            label2.TabIndex = 3;
+            label2.Text = "NOMBRE USUARIO";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(133, 78);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(272, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(133, 127);
+            textBox3.Name = "textBox3";
+            textBox3.PasswordChar = '*';
+            textBox3.Size = new Size(272, 23);
+            textBox3.TabIndex = 6;
+            textBox3.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 130);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 15);
+            label3.TabIndex = 5;
+            label3.Text = "PASSWORD";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(19, 186);
+            label4.Name = "label4";
+            label4.Size = new Size(99, 15);
+            label4.TabIndex = 7;
+            label4.Text = "FECHA REGISTRO";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(133, 180);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(272, 23);
+            dateTimePicker1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(43, 243);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 23);
+            button2.TabIndex = 9;
+            button2.Text = "GUARDAR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(270, 243);
+            button3.Name = "button3";
+            button3.Size = new Size(135, 23);
+            button3.TabIndex = 10;
+            button3.Text = "CANCELAR";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // UsuarioInsertarVista
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(417, 295);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "UsuarioInsertarVista";
+            Text = "UsuarioInsertarVista";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label3;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private Button button2;
+        private Button button3;
+    }
+}
